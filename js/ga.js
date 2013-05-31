685c7138cac09141f81ab8229766869ad90b4fb8
MgaMPKAy <mgampkay@163.com>
2013-05-31 21:43:42 +0800

    Upadate style and load ga conditionaly

--- js/ga.js
+++ js/ga.js
@@ -5,7 +5,9 @@ _gaq.push(['_setAllowLinker', true]);
 _gaq.push(['_trackPageview']);
 
 (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    if (window.location.hostname != "127.0.0.1") {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    }
 })();
