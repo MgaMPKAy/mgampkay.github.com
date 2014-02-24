0e61c824b7ecfc3aae7c08ceac2912bf4e8b240a
MgaMPKAy <mgampkay@163.com>
2014-02-24 20:58:02 +0800

    Remove jquery

--- js/post.js
+++ js/post.js
@@ -1,57 +1,62 @@
-var disqus_shortname = 'mgampkay';
-var disqus_loaded = false;
-function load_disqus() {
-    if (disqus_loaded) return;
+(function() {
+    function load_disqus() {
+	console.log("Loaded");
+	var disqus_shortname = 'mgampkay';
+	var dsqdiv = document.createElement("div");
+	dsqdiv.id = "disqus_thread";
+	document.getElementById("comment").appendChild(dsqdiv);
 
-    var dsqdiv = document.createElement("div");
-    dsqdiv.id = "disqus_thread";
-    document.getElementById("comment").appendChild(dsqdiv);
-
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-
-    disqus_loaded = true;
-}
-
-$(document).ready(function() {
-    // Add extra information to code blocks
-    $('code').each(function(){
-    	var title = $(this).attr("class");
-            if(title != '' && title !== undefined){
-                if(title.match('sourceCode')){
-                    var title = title.split(' ')[1];
-                };
-		$(this).before('<span class="language">'+title+'</span>');
-            }
-    });
-
-    // Load Disqus
-    var windowHeigh = $(window).height();
-    var bodyHeigh   = $("body").height();
-    if (window.location.hostname == "127.0.0.1") {
-        disqus_loaded = true;
-    }
-    if (bodyHeigh > windowHeigh) {
-	setTimeout(load_disqus, 1000);
-    } else {
-        load_disqus();
+	var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+	dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
     }
 
-    // Show full source code
-    $("pre").each(function(idx, elem) {
+    function add_language_tag() {
+	var codes = document.querySelectorAll("code");
+	[].forEach.call(codes, function(elem) {
+	    if (elem.classList.contains("sourceCode")) {
+		var lang = elem.className.split(' ')[1];
+		var s = document.createElement('span');
+		s.classList.add("language");
+		s.innerText = lang;
+		elem.parentNode.insertBefore(s, elem);
+	    }
+	});
+    }
 
-	var scrollWidth = elem.scrollWidth;
-	var clientWidth = elem.clientWidth;
-	if (scrollWidth > clientWidth) {
-	    $(this).mouseover(function() {
-		// Hack
-		$(this).css("width", scrollWidth + 40);
-	    });
-	    $(this).mouseout(function() {
-		// Hack
-		$(this).css("width", clientWidth - 14);
-	    });
+    function load_disqus_when_necy() {
+	if (window.location.hostname == "127.0.0.1") {
+            return;
 	}
-    });
-});
+	var windowHeigh = window.innerHeight;
+	var bodyHeigh   = document.querySelector('body').offsetHeight;
+
+	if (bodyHeigh > windowHeigh) {
+	    setTimeout(load_disqus, 129);
+	} else {
+            load_disqus();
+	}
+    }
+
+    function show_full_code() {
+	var pres = document.querySelectorAll("pre");
+	[].forEach.call(pres, function (elem) {
+	    var scrollWidth = elem.scrollWidth;
+	    var clientWidth = elem.clientWidth;
+	    console.log(scrollWidth, clientWidth);
+	    if (scrollWidth > clientWidth) {
+		elem.addEventListener('mouseover', function() {
+		    elem.style.width = scrollWidth + 40 + "px";
+		}, false);
+	    }
+	})
+    }
+
+    if ("classList" in document.documentElement) {
+	document.addEventListener("DOMContentLoaded", function() {
+	    load_disqus_when_necy();
+	    add_language_tag();
+	    show_full_code();
+	}, false);
+    }
+})()
